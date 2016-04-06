33e045c15a4f8682ea803b05ba732d68a73f20d3
ksrubka <beata.choluj@wp.pl>
2016-04-06 09:32:12 +0200

    Klasa Clip - implementacja interfejsu Product

--- src/pl/com/bottega/photostock/sales/model/products/Clip.java
+++ src/pl/com/bottega/photostock/sales/model/products/Clip.java
@@ -1,10 +1,36 @@
 package pl.com.bottega.photostock.sales.model.products;
 
 import pl.com.bottega.photostock.sales.model.Client;
+import pl.com.bottega.photostock.sales.model.Product;
 
 /**
  * Created by Beata Iłowiecka on 06.04.16.
  */
-public class Clip {
+public class Clip implements Product{
+
+    @Override
+    public boolean isAvailable() {
+        return false;
+    }
+
+    @Override
+    public double calculatePrice() {
+        return 0;
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+
+    @Override
+    public void reservePer(Client client) {
+
+    }
+
+    @Override
+    public void unreservePer(Client client) {
+
+    }
 
 }
