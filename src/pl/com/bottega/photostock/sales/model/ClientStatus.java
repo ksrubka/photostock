faa7fe5bd0cf468608a415dc63216fb903773642
ksrubka <beata.choluj@wp.pl>
2016-04-06 09:43:37 +0200

    creating body of ClientStatus enum

--- src/pl/com/bottega/photostock/sales/model/ClientStatus.java
+++ src/pl/com/bottega/photostock/sales/model/ClientStatus.java
@@ -4,4 +4,16 @@ package pl.com.bottega.photostock.sales.model;
  * Created by Beata Iłowiecka on 06.04.16.
  */
 public enum ClientStatus {
+
+    STANDARD("standardowy"), VIP("vip"), GOLD("złoty"), SILVER("srebrny"), PLATINUM("platynowy");
+
+    String polishStatus;
+
+    private ClientStatus(String polishStatus){
+        this.polishStatus = polishStatus;
+    }
+
+    public String getPolishString(){
+        return polishStatus;
+    }
 }
