c05f310f74f31af8c314c008515eda0f791d626f
ksrubka <beata.choluj@wp.pl>
2016-04-06 09:31:04 +0200

    Interfejs Product

--- src/pl/com/bottega/photostock/sales/model/Product.java
+++ src/pl/com/bottega/photostock/sales/model/Product.java
@@ -4,4 +4,15 @@ package pl.com.bottega.photostock.sales.model;
  * Created by Beata Iłowiecka on 06.04.16.
  */
 public interface Product {
+
+    boolean isAvailable();
+
+    double calculatePrice();
+
+    //make inactive
+    void cancel();
+
+    void reservePer(Client client);
+
+    void unreservePer(Client client);
 }
