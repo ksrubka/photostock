e2d7e56c02a9d674dfb3bd7bf94b3830516798b2
ksrubka <beata.choluj@wp.pl>
2016-04-07 16:44:43 +0200

    CHANNEL enum added

--- src/pl/com/bottega/photostock/sales/model/products/Song.java
+++ src/pl/com/bottega/photostock/sales/model/products/Song.java
@@ -7,23 +7,39 @@ import java.time.Duration;
  */
 public class Song extends AbstractProduct{
 
+    private enum CHANNEL{
+        STEREO("2.0"), FIVE_ONE("5.1"), SEVEN_ONE("7.1");
+
+        private String channel;
+
+        private CHANNEL(String channel){
+            this.channel = channel;
+        }
+
+        public String getChannel(){
+            return channel;
+        }
+    }
+
     private String performer;
     private String title;
     private Duration duration;
+    private CHANNEL channel;
 
-    public Song(String number, double price, String[] tags, String performer, String title, Duration duration, boolean active) {
+    public Song(String number, double price, String[] tags, String performer, String title, Duration duration, CHANNEL channel, boolean active) {
         super(number, price, tags, active);
         this.performer = performer;
         this.title = title;
         this.duration = duration;
+        this.channel = channel;
 
     }
 
-    public Song(String number, double price, String[] tags, String performer, String title, Duration duration) {
-        this(number, price, tags, performer, title, duration, true);
+    public Song(String number, double price, String[] tags, String performer, String title, Duration duration, CHANNEL channel) {
+        this(number, price, tags, performer, title, duration, channel, true);
     }
 
     public Song(){
-        this("001", 12, new String[] {"black keys", "ceiling", "gold"}, "Black keys", "Gold on the ceiling", Duration.ofSeconds(225));
+        this("001", 12, new String[] {"black keys", "ceiling", "gold"}, "Black keys", "Gold on the ceiling", Duration.ofSeconds(225), CHANNEL.STEREO);
     }
 }
