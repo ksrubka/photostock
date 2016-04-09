4dde4eb98b439a52941b0efcacfd99eaa9e7f32e
ksrubka <beata.choluj@wp.pl>
2016-04-09 09:15:42 +0200

    comments added

--- src/pl/com/bottega/photostock/sales/application/ConsoleApp.java
+++ src/pl/com/bottega/photostock/sales/application/ConsoleApp.java
@@ -32,7 +32,8 @@ public class ConsoleApp {
         // ale czy powinna móc po wygenerowniu oferty dla kogos innego? chyba nie.
         // TODO należy dodać zmiany w generateOffer() w klasie Reservation
         // np dodać pole boolean hasOfferGenerated w klasie AbstractProduct?
-        // i jeśli true to nie rezerwujemy już dla nikogo (ale to trzeba wyłączyć po wygaśnięciu oferty)
+        // i jeśli true to nie rezerwujemy już dla nikogo
+        // (ale to trzeba wyłączyć po wygaśnięciu oferty dla każdego produktu) - czy jest sens to robić? może można lepiej, inaczej?
         // TODO no właśnie - jak wygasić ofertę? i kiedy? i gdzie? w konstruktorze Purchase?
         // a jeśli canBeBoughtByMany (do zmiany zamiast to enigmatyczne !shared) to nie bierzemy pod uwage tego hasOfferGenerated
         // a tak wogóle to mi się nie podoba bo zmiana jednej klasy przez drugą, nie wiem czy nie za dużo mieszania?
