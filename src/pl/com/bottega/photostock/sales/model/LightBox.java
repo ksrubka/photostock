fd2efe4c7244776039c1729bde809d4b80a95957
ksrubka <beata.choluj@wp.pl>
2016-04-09 13:44:59 +0200

    commented legacy code removed

--- src/pl/com/bottega/photostock/sales/model/LightBox.java
+++ src/pl/com/bottega/photostock/sales/model/LightBox.java
@@ -24,59 +24,6 @@ public class LightBox {
         this.closed = true;
     }
 
-    /* public void add(Picture pictureToAdd) throws IllegalStateException , IllegalArgumentException {
-        boolean added = false;
-        int cursor = 0;
-        for (Picture p : items){
-            if (p != null){
-                String nr1 = p.getNumber();
-                String nr2 = pictureToAdd.getNumber();
-
-                if (nr1.equals(nr2)) {
-                    throw new IllegalArgumentException("LightBox already contains picture " + nr2);
-                }
-            }
-            else {
-                // p = pictureToAdd; // p jest innym miejscem niż kratka tablicy!
-                items[cursor] = pictureToAdd;
-                added = true;
-                break;
-                }
-            }
-            if (!added){
-            new IllegalStateException("LightBox przepełniony!");
-        }*/
-
-        /*int cursor = 0;
-
-        while (cursor < items.length){
-            Picture pic = items[cursor];
-            if (pic != null){
-                String nr1 = pic.getNumber();
-                String nr2 = pictureToAdd.getNumber();
-
-                if (nr1.equals(nr2)){
-                     throw new IllegalArgumentException("LightBox already contains picture " + nr2);
-                }
-            }
-            cursor++;
-        }*/
-
-        /*boolean added = false;
-
-        for (int i = 0; i < items.length; i++){
-            Picture reference = items[i];
-            if (reference == null){
-              items[i] = pictureToAdd;
-                added = true;
-                break;
-            }
-        }
-        if (!added){
-            new IllegalStateException("LightBox przepełniony!");
-        }
-    }*/
-
     public void add(Product... products)throws IllegalStateException, IllegalArgumentException {
 
         validate();
