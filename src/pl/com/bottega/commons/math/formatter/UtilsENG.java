5e2d75efdb150fa5725a056c817fc04ea43ce0ed
ksrubka <beata.choluj@wp.pl>
2016-04-12 16:21:52 +0200

    new FormattingLanguage available

--- src/pl/com/bottega/commons/math/formatter/UtilsENG.java
+++ src/pl/com/bottega/commons/math/formatter/UtilsENG.java
@@ -1,7 +1,83 @@
 package pl.com.bottega.commons.math.formatter;
 
+import java.util.ArrayList;
+
 /**
  * Created by Beata Iłowiecka on 11.04.16.
  */
 public class UtilsENG {
+
+    public static final String[] TENS = {"", "", "twenty", "thirty", "fourty", "fifty", "sixty",
+            "seventy", "eighty", "ninety"};
+
+    public static final String[] SEVERALS = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+            "seventeen", "eighteen", "nineteen"};
+
+    public static final String[] BIG_NUMBERS =
+            {"", "thousand,", "million,", "billion,", "trillion,", "quadrillion,", "quintillion,"};
+
+    public static String formatBigNumber(ArrayList<ArrayList<Byte>> arrayOfArrays){
+        StringBuilder result = new StringBuilder();
+        int index = arrayOfArrays.size() - 1;
+
+        for (ArrayList<Byte> byteArray : arrayOfArrays){
+            String formattedHundred = formatHundred(byteArray);
+
+            result.append(formattedHundred);
+
+            // append name of a big number
+            // if number is 0, don't write it's name
+            if (byteArray.get(0) == 0 && byteArray.get(1) == 0 && byteArray.get(2) == 0) {
+                index--;
+                continue;
+            }
+            result.append(BIG_NUMBERS[index--]);
+            result.append(" ");
+        }
+        return result.toString();
+    }
+
+    // in: {0,0,1}          out: jeden
+    // in: {1,0,5}          out: sto pięć
+    // in: {3,2,9}          out: trzysta dwadzieścia dziewięć
+    public static String formatHundred(ArrayList<Byte> byteArray){
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < 3; i++){
+            byte currentDigit = byteArray.get(i);
+
+            if (currentDigit != 0){
+                switch (i){
+                    case 0:
+                        result.append(SEVERALS[currentDigit]);
+                        result.append(" hundred");
+                        break;
+                    case 1:
+                        if (currentDigit == 1) {
+                            String tempResult = String.valueOf(1) + String.valueOf(byteArray.get(i + 1));
+                            int several = Integer.parseInt(tempResult);
+                            result.append(SEVERALS[several]);
+                            // break for loop
+                            i = 2;
+                        }
+                        else {
+                            result.append(TENS[currentDigit]);
+                            if (byteArray.get(i + 1) != 0){
+                                result.append("-");
+                                continue;
+                            }
+                        }
+                        break;
+                    case 2:
+                        result.append(SEVERALS[currentDigit]);
+                        break;
+                    default:
+                        break;
+                }
+                result.append(" ");
+            }
+        }
+        return result.toString();
+    }
 }
