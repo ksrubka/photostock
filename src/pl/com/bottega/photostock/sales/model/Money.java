13b106ba21a828f5f320a716d3eadc48e9d12c6b
ksrubka <beata.choluj@wp.pl>
2016-04-15 21:19:44 +0200

    Add methods, change fields type

--- src/pl/com/bottega/photostock/sales/model/Money.java
+++ src/pl/com/bottega/photostock/sales/model/Money.java
@@ -1,5 +1,8 @@
 package pl.com.bottega.photostock.sales.model;
 
+import pl.com.bottega.commons.math.Fraction;
+
+import java.util.Currency;
 import java.util.Date;
 
 /**
@@ -7,17 +10,53 @@ import java.util.Date;
  */
 public class Money {
 
-    private double value;
-    private String currency;
+    private final Fraction value = null;
+    private final Currency currency = null;
 
-    public Money(double value, String currency) {
-        this.value = value;
-        this.currency = currency;
+    public Money(Double value, String currency) {
+        //TODO
     }
 
-    public Money(double value){
+    public Money(int integerValue, int cents, String currency) {
+        //TODO
+
+    }
+
+    public Money(double value) {
         this(value, "PLN");
     }
 
+    public Money add(Money amount){
+        return null;//TODO
+    }
 
+    public Money substract(Money amount){
+        return null;//TODO
+    }
+
+    public Money multiple(int ratio){
+        return null;//TODO
+    }
+
+    public Money multiple(double ratio){
+        return null;//TODO
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
+
+        if (!value.equals(money.value)) return false;
+        return currency.equals(money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = value.hashCode();
+        result = 31 * result + currency.hashCode();
+        return result;
+    }
 }
