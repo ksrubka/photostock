27149e23d137ba8c883c8a0cd6155dc675549243
ksrubka <beata.choluj@wp.pl>
2016-04-17 21:56:58 +0200

    Add fields, constructor, getters

--- src/pl/com/bottega/photostock/sales/model/ProductNotAvailableException.java
+++ src/pl/com/bottega/photostock/sales/model/ProductNotAvailableException.java
@@ -4,4 +4,25 @@ package pl.com.bottega.photostock.sales.model;
  * Created by Beata Iłowiecka on 16.04.16.
  */
 public class ProductNotAvailableException extends RuntimeException {
+
+    // powinien nieść info o błędzie
+    // komunikat - opis błędu
+    // nr i typ obiektu biznesowego
+
+    private String number;
+    private Class clazz;
+
+    public ProductNotAvailableException(String message, String number, Class clazz){
+        super(message);
+        this.number = number;
+        this.clazz = clazz;
+    }
+
+    public Class getClazz(){
+        return clazz;
+    }
+
+    public String getNumber() {
+        return number;
+    }
 }
