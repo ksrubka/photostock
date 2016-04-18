74326351ecb869a83bc1b8bb32f365057515cf46
ksrubka <beata.choluj@wp.pl>
2016-04-18 18:02:55 +0200

    Create equals(Money)

--- src/pl/com/bottega/photostock/sales/model/Money.java
+++ src/pl/com/bottega/photostock/sales/model/Money.java
@@ -10,16 +10,16 @@ import java.util.Date;
  */
 public class Money {
 
-    private final Fraction value = null;
-    private final Currency currency = null;
+    private final Double value;//TODO zamienić na Fraction
+    private final String currency;//TODO zamienić na użycie klasy Currency z biblioteki Javy
 
     public Money(Double value, String currency) {
-        //TODO
+        this.value = value;
+        this.currency = currency;
     }
 
     public Money(int integerValue, int cents, String currency) {
-        //TODO
-
+        this(integerValue + (double)cents/100, currency);
     }
 
     public Money(double value) {
@@ -27,11 +27,15 @@ public class Money {
     }
 
     public Money add(Money amount){
-        return null;//TODO
+        if (!currency.equals(amount.currency))
+            throw new IllegalArgumentException("Can not add if different currency");
+        return new Money(value + amount.value, currency);
     }
 
     public Money substract(Money amount){
-        return null;//TODO
+        if (!currency.equals(amount.currency))
+            throw new IllegalArgumentException("Can not add if different currency");
+        return new Money(value - amount.value, currency);
     }
 
     public Money multiple(int ratio){
@@ -42,15 +46,35 @@ public class Money {
         return null;//TODO
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
+    public boolean equals(Money m2) {
+        /*if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         Money money = (Money) o;
 
         if (!value.equals(money.value)) return false;
-        return currency.equals(money.currency);
+        return currency.equals(money.currency);*/
+// ===============================================================
+        if (this == m2) return true;
+        else {
+            if (this.currency == m2.currency){
+                double delta;
+                double smallerAmount;
+                double biggerAmount;
+
+                if (this.value - m2.value < 0){
+                    smallerAmount = this.value;
+                    biggerAmount = m2.value;
+                }
+                else {
+                    smallerAmount = m2.value;
+                    biggerAmount = this.value;
+                }
+                delta = (smallerAmount > 100) ? 0.001 : 0.01;
+                return  (biggerAmount - smallerAmount < delta);
+            }
+        }
+        return false;
     }
 
     @Override
@@ -59,4 +83,46 @@ public class Money {
         result = 31 * result + currency.hashCode();
         return result;
     }
+
+    /**
+     *
+     * @param val
+     * @return true if this is grater or equals than val
+     */
+    public boolean ge(Money val) {
+        return this.value >= val.value;
+    }
+
+    /**
+     *
+     * @param val
+     * @return true if this is less or equals than val
+     */
+    public boolean le(Money val) {
+        return this.value <= val.value;
+    }
+
+    /**
+     *
+     * @param val
+     * @return  true if this is less than val
+     */
+    public boolean lt(Money val) {
+        return this.value < val.value;
+    }
+
+    /**
+     *
+     * @param val
+     * @return  true if this is greater than val
+     */
+    public boolean gt(Money val) {
+        return this.value > val.value;
+    }
+
+    public Money getZero(){
+        return new Money(0d, currency);
+    }
+
+
 }
