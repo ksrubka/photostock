15b498b8b22962a315c51757e371afa113f4648e
ksrubka <beata.choluj@wp.pl>
2016-04-21 16:04:35 +0200

    Create empty methods

--- src/pl/com/bottega/photostock/sales/model/StandardChargingStrategy.java
+++ src/pl/com/bottega/photostock/sales/model/StandardChargingStrategy.java
@@ -4,4 +4,48 @@ package pl.com.bottega.photostock.sales.model;
  * Created by Beata Iłowiecka on 21.04.16.
  */
 public class StandardChargingStrategy implements ChargingStrategy {
+
+    @Override
+    public boolean canAfford(Money productCost) {
+        return false;
+    }
+
+    @Override
+    public void charge(Money productCost, String cause) {
+
+    }
+
+    @Override
+    public void recharge(Money amount) {
+
+    }
+
+    @Override
+    public Money getSaldo() {
+        return null;
+    }
+
+
+
+    /*public boolean canAfford(double money) {
+        return  amount >= money;
+    }
+
+    public void charge(double pictureCost, String cause){ //TODO what to do with 'cause'?
+
+        if (canAfford(pictureCost)) {
+            amount -= pictureCost;
+        }
+        else {
+            throw new IllegalStateException("Sorry, you can not afford this product.");
+        }
+    }
+
+    public void recharge(double amount){
+        this.amount += amount;
+    }
+
+    public double getSaldo(){
+        return amount;
+    }*/
 }
--- src/pl/com/bottega/photostock/sales/model/VIPChargingStrategy.java
+++ src/pl/com/bottega/photostock/sales/model/VIPChargingStrategy.java
@@ -4,4 +4,71 @@ package pl.com.bottega.photostock.sales.model;
  * Created by Beata Iłowiecka on 21.04.16.
  */
 public class VIPChargingStrategy implements ChargingStrategy {
+
+    @Override
+    public boolean canAfford(Money productCost) {
+        return false;
+    }
+
+    @Override
+    public void charge(Money productCost, String cause) {
+
+    }
+
+    @Override
+    public void recharge(Money amount) {
+
+    }
+
+    @Override
+    public Money getSaldo() {
+        return null;
+    }
+
+    /*@Override
+    public boolean canAfford(double money) {
+
+        double purchasePotential = amount + creditLimit;
+        return  purchasePotential >= money;
+    }
+
+    @Override
+    public void charge(double pictureCost, String cause){ //TODO what to do with 'cause'?
+
+        if (canAfford(pictureCost)){
+            if (amount >= pictureCost){
+                amount -= pictureCost;
+            } else {
+                if ((pictureCost - amount) <= creditLimit){
+                    debt = pictureCost - amount;
+                    amount = 0;
+                } else return;
+            }
+        }
+    }
+
+    @Override
+    public void recharge(double amount){
+        if (this.amount >= 0){
+            this.amount += amount;
+        }
+        else {
+            if (debt >= amount) {
+                debt -= amount;
+            } else {
+                this.amount += amount - debt;
+                debt = 0;
+            }
+        }
+    }
+
+    @Override
+    public double getSaldo(){
+        if (amount >= 0){
+            return amount;
+        }
+        else {
+            return -debt;
+        }
+    }*/
 }
