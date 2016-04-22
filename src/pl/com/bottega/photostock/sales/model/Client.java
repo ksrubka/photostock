2dcae60ae2c269d224bbd9e7c49b009308877c90
ksrubka <beata.choluj@wp.pl>
2016-04-22 16:31:43 +0200

    Change the constructor and some methods parameters

--- src/pl/com/bottega/photostock/sales/model/Client.java
+++ src/pl/com/bottega/photostock/sales/model/Client.java
@@ -1,5 +1,6 @@
 package pl.com.bottega.photostock.sales.model;
 
+import pl.com.bottega.photostock.sales.infrastructure.repositories.factory.StrategyFactory;
 import pl.com.bottega.photostock.sales.model.client_strategies.ChargingData;
 import pl.com.bottega.photostock.sales.model.client_strategies.ChargingStrategy;
 import pl.com.bottega.photostock.sales.model.client_strategies.StandardChargingStrategy;
@@ -17,25 +18,28 @@ public class Client {
     private ChargingStrategy chargingStrategy;
     private ClientStatus status;
     private Money amount;
+
+    private Money debt;
+    private Money creditLimit;
     private boolean active;
 
-
-    public Client(String name, String address, ClientStatus status, double amount, boolean active) {
+    public Client(String name, String address, ClientStatus status, double amount, double debt, double creditLimit, boolean active) {
         this.name = name;
         this.address = address;
         this.status = status;
-        this.chargingStrategy = ;//todo chargingStrategyFactory
-                //(status == ClientStatus.VIP) ? new VIPChargingStrategy() : new StandardChargingStrategy();
+        this.chargingStrategy = StrategyFactory.create(status);
         this.amount = new Money(amount);
+        this.debt = new Money(debt);
+        this.creditLimit = new Money(creditLimit);
         this.active = active;
     }
 
     public Client(String name, String address, double amount) {
-        this(name, address, ClientStatus.STANDARD, amount, true);
+        this(name, address, ClientStatus.STANDARD, amount, 0, 0, true);
     }
 
     public Client() {
-        this("Helena Ferenc", "Księżyc", ClientStatus.STANDARD, 500, true);
+        this("Helena Ferenc", "Księżyc", ClientStatus.STANDARD, 500, 0, 0, true);
     }
 
     public String getName() {
@@ -59,27 +63,35 @@ public class Client {
         return name + " - " + status.getPolishString();
     }
 
-    public boolean canAfford(Money money) {
-        return chargingStrategy.canAfford(money);
+    public boolean canAfford(Money productCost, ChargingData chargingData) {
+        return chargingStrategy.canAfford(productCost, this.generateData());
     }
 
     public void charge(Money productCost, String cause){ //TODO what to do with 'cause'?
-        chargingStrategy.charge(productCost, cause);
+        chargingStrategy.charge(productCost, cause, this.generateData());
     }
 
     public void recharge(Money amount){
-        chargingStrategy.recharge(amount);
+        chargingStrategy.recharge(amount, this.generateData());
     }
 
     public Money getSaldo(){
-        return chargingStrategy.getSaldo();
+        return chargingStrategy.getSaldo(this.generateData());
     }
 
     public ChargingData generateData() {
-        return new ChargingData(amount);
+        return new ChargingData(this);
     }
 
     public Money getAmount() {
         return amount;
+    }
+
+    public Money getDebt() {
+        return debt;
+    }
+
+    public Money getCreditLimit() {
+        return creditLimit;
     }
 }
