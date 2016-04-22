a86228a6f390cf7560253dafc205f6f1e3ea07c4
ksrubka <beata.choluj@wp.pl>
2016-04-22 20:58:47 +0200

    Create factory for ApprovingStrategy used in Reservation constructor

---
+++ src/pl/com/bottega/photostock/sales/infrastructure/repositories/factory/ApprovingFactory.java
@@ -0,0 +1,19 @@
+package pl.com.bottega.photostock.sales.infrastructure.repositories.factory;
+
+import pl.com.bottega.photostock.sales.model.ClientStatus;
+import pl.com.bottega.photostock.sales.model.client_strategies.ApprovingStrategy;
+import pl.com.bottega.photostock.sales.model.client_strategies.StandardApprovingStrategy;
+import pl.com.bottega.photostock.sales.model.client_strategies.VIPApprovigStrategy;
+
+/**
+ * Created by Beata Iłowiecka on 22.04.16.
+ */
+public class ApprovingFactory {
+
+    public static ApprovingStrategy create(ClientStatus status){
+        if (status == ClientStatus.VIP)
+            return new VIPApprovigStrategy();
+        else
+            return  new StandardApprovingStrategy();
+    }
+}
