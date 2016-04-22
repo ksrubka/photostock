6f8c180a4c0d216bf78d206f500fde4006a98b82
ksrubka <beata.choluj@wp.pl>
2016-04-22 16:23:28 +0200

    Create StarategyFactory for Client

---
+++ src/pl/com/bottega/photostock/sales/infrastructure/repositories/factory/StrategyFactory.java
@@ -0,0 +1,18 @@
+package pl.com.bottega.photostock.sales.infrastructure.repositories.factory;
+
+import pl.com.bottega.photostock.sales.model.client_strategies.ChargingStrategy;
+import pl.com.bottega.photostock.sales.model.ClientStatus;
+import pl.com.bottega.photostock.sales.model.client_strategies.StandardChargingStrategy;
+import pl.com.bottega.photostock.sales.model.client_strategies.VIPChargingStrategy;
+
+/**
+ * Created by Beata Iłowiecka on 22.04.16.
+ */
+public class StrategyFactory {
+    public static ChargingStrategy create(ClientStatus clientStatus){
+        if (clientStatus == ClientStatus.VIP)
+            return new VIPChargingStrategy();
+        else
+            return new StandardChargingStrategy();
+    }
+}
