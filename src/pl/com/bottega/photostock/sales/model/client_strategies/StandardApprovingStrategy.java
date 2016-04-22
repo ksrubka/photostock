ed5e61f86455462991d3263027a418bf9af4995e
ksrubka <beata.choluj@wp.pl>
2016-04-22 21:19:12 +0200

    Create ApprovingStrategy interface + implementing classes

---
+++ src/pl/com/bottega/photostock/sales/model/client_strategies/StandardApprovingStrategy.java
@@ -0,0 +1,9 @@
+package pl.com.bottega.photostock.sales.model.client_strategies;
+
+import pl.com.bottega.photostock.sales.infrastructure.repositories.factory.ApprovingFactory;
+
+/**
+ * Created by Beata Iłowiecka on 22.04.16.
+ */
+public class StandardApprovingStrategy implements ApprovingStrategy {
+}
