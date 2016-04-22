4d0c820290b97bffb9778acc0e5b3650bf60b529
ksrubka <beata.choluj@wp.pl>
2016-04-22 17:06:12 +0200

    Add ChargingData as param to create()

--- src/pl/com/bottega/photostock/sales/infrastructure/repositories/factory/StrategyFactory.java
+++ src/pl/com/bottega/photostock/sales/infrastructure/repositories/factory/StrategyFactory.java
@@ -1,6 +1,7 @@
 package pl.com.bottega.photostock.sales.infrastructure.repositories.factory;
 
 import pl.com.bottega.photostock.sales.model.client_strategies.ChargingStrategy;
+import pl.com.bottega.photostock.sales.model.client_strategies.ChargingData;
 import pl.com.bottega.photostock.sales.model.ClientStatus;
 import pl.com.bottega.photostock.sales.model.client_strategies.StandardChargingStrategy;
 import pl.com.bottega.photostock.sales.model.client_strategies.VIPChargingStrategy;
@@ -9,9 +10,9 @@ import pl.com.bottega.photostock.sales.model.client_strategies.VIPChargingStrate
  * Created by Beata Iłowiecka on 22.04.16.
  */
 public class StrategyFactory {
-    public static ChargingStrategy create(ClientStatus clientStatus){
+    public static ChargingStrategy create(ClientStatus clientStatus, ChargingData chargingData){
         if (clientStatus == ClientStatus.VIP)
-            return new VIPChargingStrategy();
+            return new VIPChargingStrategy(chargingData);
         else
             return new StandardChargingStrategy();
     }
