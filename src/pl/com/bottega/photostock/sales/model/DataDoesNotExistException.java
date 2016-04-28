51d7a32584477fa0b68ce16337aa448621925e22
ksrubka <beata.choluj@wp.pl>
2016-04-28 19:37:59 +0200

    Create exception class

---
+++ src/pl/com/bottega/photostock/sales/model/DataDoesNotExistException.java
@@ -0,0 +1,24 @@
+package pl.com.bottega.photostock.sales.model;
+
+/**
+ * Created by Beata Iłowiecka on 28.04.16.
+ */
+public class DataDoesNotExistException extends RuntimeException {
+
+    private String number;
+    private Class clazz;
+
+    public DataDoesNotExistException(String message, String number, Class clazz){
+        super(message);
+        this.number = number;
+        this.clazz = clazz;
+    }
+
+    public Class getClazz(){
+        return clazz;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
