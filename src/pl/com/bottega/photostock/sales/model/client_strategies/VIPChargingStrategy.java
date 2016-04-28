85f646e090389433573e0da5f456e0d468c1bed8
ksrubka <beata.choluj@wp.pl>
2016-04-28 13:16:44 +0200

    Add debt and creditLimit fields and add charging  to methods params
    Which greatly influence methods logic - now it makes operations on actual client, at last.

--- src/pl/com/bottega/photostock/sales/model/client_strategies/VIPChargingStrategy.java
+++ src/pl/com/bottega/photostock/sales/model/client_strategies/VIPChargingStrategy.java
@@ -1,41 +1,35 @@
 package pl.com.bottega.photostock.sales.model.client_strategies;
 
-import pl.com.bottega.commons.math.Fraction;
-import pl.com.bottega.photostock.sales.model.Client;
 import pl.com.bottega.photostock.sales.model.Money;
-import pl.com.bottega.photostock.sales.model.client_strategies.ChargingStrategy;
 
 /**
  * Created by Beata Iłowiecka on 21.04.16.
  */
 public class VIPChargingStrategy implements ChargingStrategy {
 
-    Money amount;
     Money debt;
     Money creditLimit;
 
-    public VIPChargingStrategy(ChargingData chargingData){
-        this.amount = chargingData.amount;
-        this.debt = chargingData.debt;
-        this.creditLimit = chargingData.creditLimit;
-
+    public VIPChargingStrategy(){
+        this.debt = new Money(0);
+        this.creditLimit = new Money(200); //todo do ustalenia czy każdy vip ma tyle sam czy może każdy inaczej
     }
 
     @Override
-    public boolean canAfford(Money money) {
-        Money purchasePotential = amount.add(creditLimit);
-        return  purchasePotential.greaterOrEqual(money);
+    public boolean canAfford(Charging charging, Money newAmount) {
+        Money purchasePotential = charging.getAmount().add(creditLimit);
+        return  purchasePotential.greaterOrEqual(newAmount);
     }
 
     @Override
-    public void charge(Money productCost, String cause){ //TODO what to do with 'cause'?
-        if (canAfford(productCost)){
-            if (amount.greaterOrEqual(productCost)){
-                amount = amount.substract(productCost); // wyrażenie po znaku '=' nie odejmuje od amount ale zwraca nowy obiekt
+    public void charge(Charging charging, Money productCost, String cause){ //TODO what to do with 'cause'?
+        if (canAfford(charging, productCost)){
+            if (charging.getAmount().greaterOrEqual(productCost)){
+                charging.setAmount(charging.getAmount().substract(productCost)); // wyrażenie po znaku '=' nie odejmuje od amount ale zwraca nowy obiekt
             } else {
-                if ((productCost.substract(amount)).lowerOrEqual(creditLimit)){
-                    debt = productCost.substract(amount);
-                    amount = amount.getZero();
+                if ((productCost.substract(charging.getAmount())).lowerOrEqual(creditLimit)){
+                    debt = productCost.substract(charging.getAmount());
+                    charging.setAmount(charging.getAmount().getZero());
                 }
             }
         }
@@ -43,24 +37,24 @@ public class VIPChargingStrategy implements ChargingStrategy {
     }
 
     @Override
-    public void recharge(Money amount){
-        if (this.amount.greaterOrEqual(amount.getZero())){
-            this.amount = this.amount.add(amount);
+    public void recharge(Charging charging, Money newAmount){
+        if (charging.getAmount().greaterOrEqual(newAmount.getZero())){
+            charging.setAmount(charging.getAmount().add(newAmount));
         }
         else {
-            if (debt.greaterOrEqual(amount)) {
-                debt = debt.substract(amount);
+            if (debt.greaterOrEqual(newAmount)) {
+                debt = debt.substract(newAmount);
             } else {
-                this.amount = this.amount.add(amount.substract(debt));
+                charging.setAmount(charging.getAmount().add(newAmount.substract(debt)));
                 debt = debt.getZero();
             }
         }
     }
 
     @Override
-    public Money getSaldo(){
-        if (amount.greaterOrEqual(amount.getZero())){
-            return amount;
+    public Money getSaldo(Charging charging){
+        if (charging.getAmount().greaterOrEqual(charging.getAmount().getZero())){
+            return charging.getAmount();
         }
         else {
             return new Money(-(debt.getDoubleValue()));
