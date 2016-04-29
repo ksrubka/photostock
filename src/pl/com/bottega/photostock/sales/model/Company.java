55ac624f538f5121b4f017922c75cbf134afd3e2
ksrubka <beata.choluj@wp.pl>
2016-04-29 19:33:25 +0200

    Create Company class

---
+++ src/pl/com/bottega/photostock/sales/model/Company.java
@@ -0,0 +1,16 @@
+package pl.com.bottega.photostock.sales.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Beata Iłowiecka on 29.04.16.
+ */
+public class Company {
+
+    private String name;
+    private String number;
+    private List<Client> employee = new ArrayList<>();
+
+
+}
