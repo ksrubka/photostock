88ba1a5a1b813df82555d61e9531224607cd0d6c
ksrubka <beata.choluj@wp.pl>
2016-04-29 18:02:00 +0200

    Create ProductIsNotAPictureException

--- src/pl/com/bottega/photostock/sales/model/exceptions/ProductIsNotAPictureException.java
+++ src/pl/com/bottega/photostock/sales/model/exceptions/ProductIsNotAPictureException.java
@@ -3,5 +3,21 @@ package pl.com.bottega.photostock.sales.model.exceptions;
 /**
  * Created by Beata Iłowiecka on 29.04.16.
  */
-public class ProductIsNotAPictureException {
+public class ProductIsNotAPictureException extends RuntimeException {
+    private String number;
+    private Class clazz;
+
+    public ProductIsNotAPictureException(String message, String number, Class clazz){
+        super(message);
+        this.number = number;
+        this.clazz = clazz;
+    }
+
+    public Class getClazz(){
+        return clazz;
+    }
+
+    public String getNumber() {
+        return number;
+    }
 }
