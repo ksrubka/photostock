1100fab4cecad17ba25278f7f6d6e2113b855df2
ksrubka <beata.choluj@wp.pl>
2016-04-29 17:51:45 +0200

    Create exception class

---
+++ src/pl/com/bottega/photostock/sales/model/ClientDoesNotExistException.java
@@ -0,0 +1,18 @@
+package pl.com.bottega.photostock.sales.model;
+
+/**
+ * Created by Beata Iłowiecka on 29.04.16.
+ */
+public class ClientDoesNotExistException extends RuntimeException {
+
+    private String name;
+
+    public ClientDoesNotExistException(String message, String name){
+        super(message);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
