b0bbf724f453880c1b74c825c922c9edb7918d28
ksrubka <beata.choluj@wp.pl>
2016-05-02 22:07:42 +0200

    Create methods signatures

--- src/pl/com/bottega/photostock/sales/api/ClientManagement.java
+++ src/pl/com/bottega/photostock/sales/api/ClientManagement.java
@@ -1,7 +1,28 @@
 package pl.com.bottega.photostock.sales.api;
 
+import pl.com.bottega.photostock.sales.model.Client;
+import pl.com.bottega.photostock.sales.model.Purchase;
+import pl.com.bottega.photostock.sales.model.Reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Beata Iłowiecka on 02.05.16.
  */
 public class ClientManagement {
+
+    public void register(String name, String surname, String address){
+
+    }
+
+    public List<Reservation> findReservations(String clientNr){
+        List<Reservation> reservations = new ArrayList<>();
+        return reservations;
+    }
+
+    public List<Purchase> findPurrchases(String clientNr){
+        List<Purchase> purchases = new ArrayList<>();
+        return purchases;
+    }
 }
