43df3c76cbecc6fdb925a1926edbf9daf390016a
ksrubka <beata.choluj@wp.pl>
2016-05-02 21:41:30 +0200

    Create ProductFactory class
    For AdminPanel create()

---
+++ src/pl/com/bottega/photostock/sales/model/client_factories/ProductFactory.java
@@ -0,0 +1,27 @@
+package pl.com.bottega.photostock.sales.model.client_factories;
+
+import pl.com.bottega.photostock.sales.model.Product;
+import pl.com.bottega.photostock.sales.model.products.*;
+
+import java.time.Duration;
+
+import static pl.com.bottega.photostock.sales.model.products.CHANNEL.STEREO;
+
+/**
+ * Created by Beata Iłowiecka on 02.05.16.
+ */
+public class ProductFactory {
+
+    public static Product create(String name, String productNumber, double price, String[] tags, PRODUCT_TYPE productType) {
+        switch (productType){
+            case PICTURE:
+                return new Picture(name, productNumber, price, tags);
+            case CLIP:
+                return new Clip(name, productNumber, price, tags, Duration.ZERO);
+            case SONG:
+                return  new Song(name, productNumber, price, tags, "", "", Duration.ZERO, STEREO);
+            default:
+                throw new IllegalArgumentException("Podany typ produktu nie istnieje");
+        }
+    }
+}
