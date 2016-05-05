6b10ff7b052417458743e08da12e705eb5064add
ksrubka <beata.choluj@wp.pl>
2016-05-05 16:45:55 +0200

    Implement promoteClient()

--- src/pl/com/bottega/photostock/sales/api/AdminPanel.java
+++ src/pl/com/bottega/photostock/sales/api/AdminPanel.java
@@ -1,11 +1,13 @@
 package pl.com.bottega.photostock.sales.api;
 
+import pl.com.bottega.photostock.sales.infrastructure.repositories.ClientRepository;
+import pl.com.bottega.photostock.sales.infrastructure.repositories.FakeClientRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.FakeProductRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.ProductRepository;
 import pl.com.bottega.photostock.sales.model.Client;
 import pl.com.bottega.photostock.sales.model.Product;
 import pl.com.bottega.photostock.sales.model.client_factories.ProductFactory;
-import pl.com.bottega.photostock.sales.model.products.PRODUCT_TYPE;
+import pl.com.bottega.photostock.sales.model.products.ProductType;
 
 import java.util.UUID;
 
@@ -15,15 +17,18 @@ import java.util.UUID;
 public class AdminPanel {
 
     private ProductRepository productRepository = new FakeProductRepository();
+    private ClientRepository clientRepository = new FakeClientRepository();
 
-    public void addProduct(String name, double price, String[] tags, PRODUCT_TYPE productType){
+    public void addProduct(String name, double price, String[] tags, ProductType productType){
         String productNumber = UUID.randomUUID().toString();
         Product product = ProductFactory.create(name, productNumber, price, tags, productType);
         productRepository.save(product);
     }
 
     public void promoteClient(String clientNr){
-
+        Client client = clientRepository.load(clientNr);
+        client.setVipStatus();
+        clientRepository.save(client);
     }
 
     public void changeCreditLimit(String clientNr){
