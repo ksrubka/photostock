ec0be46798475316b7160b9b350a03388b7a50e2
ksrubka <beata.choluj@wp.pl>
2016-05-11 21:46:05 +0200

    Implement add()

--- src/pl/com/bottega/photostock/sales/api/PurchaseProcess.java
+++ src/pl/com/bottega/photostock/sales/api/PurchaseProcess.java
@@ -2,6 +2,7 @@ package pl.com.bottega.photostock.sales.api;
 
 import pl.com.bottega.photostock.sales.infrastructure.repositories.*;
 import pl.com.bottega.photostock.sales.model.*;
+import pl.com.bottega.photostock.sales.model.exceptions.DataDoesNotExistException;
 
 /**
  * Created by Beata Iłowiecka on 23.04.2016.
@@ -13,20 +14,25 @@ public class PurchaseProcess {
     private ProductRepository productRepository = new FakeProductRepository();
     private PurchaseRepository purchaseRepository = new FakePurchaseRepository();
 
-    public String createReservation(String clientNr){
+    //add product to the reservation
+    public void add(String clientNr, String productNr) {
+        Client client = clientRepository.load(clientNr);
+        Reservation reservation = reservationRepository.load(client);
+        Product product = productRepository.load(productNr);
+        if (reservation.isClosed()) {
+            String reservationNr = createReservation(clientNr);
+            reservation = reservationRepository.load(reservationNr);
+        }
+        product.reservePer(client);
+        reservation.add(product);
+        reservationRepository.save(reservation);
+        productRepository.save(product);
+    }
+    private String createReservation(String clientNr){
         Client client = clientRepository.load(clientNr);
         Reservation reservation = new Reservation(client);
         reservationRepository.save(reservation);
         return  reservation.getNumber();
-    }
-
-    //add product to the reservation
-    public void add(String reservationNr, String productNr){
-        Reservation reservation = reservationRepository.load(reservationNr);
-        Product product = productRepository.load(productNr);
-        reservation.add(product);
-        reservationRepository.save(reservation);
-        productRepository.save(product);
     }
 
     public Offer calculateOffer(String reservationNr) {
