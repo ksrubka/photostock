d3a087f0d27147f6fdddeaa4d276a2f4d8d5861f
ksrubka <beata.choluj@wp.pl>
2016-05-11 09:42:38 +0200

    Add tests

---
+++ test/pl/com/bottega/photostock/sales/model/LightBoxTest.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.photostock.sales.model;
+
+/**
+ * Created by Beata Iłowiecka on 11.05.16.
+ */
+public class LightBoxTest {
+}
---
+++ test/pl/com/bottega/photostock/sales/model/products/PictureTest.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.photostock.sales.model.products;
+
+/**
+ * Created by Beata Iłowiecka on 11.05.16.
+ */
+public class PictureTest {
+}
