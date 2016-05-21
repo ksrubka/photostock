eee2cfad2047b8b90290271b485b671c14c9a9d7
ksrubka <beata.choluj@wp.pl>
2016-05-21 17:58:46 +0200

    Remove tags from constructor

--- src/pl/com/bottega/photostock/sales/model/products/Song.java
+++ src/pl/com/bottega/photostock/sales/model/products/Song.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 /**
  * Created by Beata Iłowiecka on 06.04.16.
  */
-public class Song extends AbstractProduct{
+public class Song extends AbstractProduct {
 
     private String performer;
     private String title;
@@ -18,14 +18,13 @@ public class Song extends AbstractProduct{
         this.title = title;
         this.duration = duration;
         this.channel = channel;
-
     }
 
-    public Song(String number, double price, String[] tags, String performer, String title, Duration duration, Channel channel) {
+    public Song(String number, double price, String performer, String title, Duration duration, Channel channel) {
         this(number, price, performer, title, duration, channel, true);
     }
 
-    public Song(){
-        this("nr6", 12, new String[] {"black keys", "ceiling", "gold"}, "Black keys", "Gold on the ceiling", Duration.ofSeconds(225), Channel.STEREO);
+    public Song() {
+        this("nr6", 12, "Black keys", "Gold on the ceiling", Duration.ofSeconds(225), Channel.STEREO);
     }
 }
