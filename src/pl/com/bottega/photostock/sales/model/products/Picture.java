dda07d8ae5c8dda2fd57697b9a2cc35e1dcdcdd4
ksrubka <beata.choluj@wp.pl>
2016-05-21 17:08:09 +0200

    Remove name

--- src/pl/com/bottega/photostock/sales/model/products/Picture.java
+++ src/pl/com/bottega/photostock/sales/model/products/Picture.java
@@ -1,24 +1,19 @@
 package pl.com.bottega.photostock.sales.model.products;
 
-import pl.com.bottega.photostock.sales.model.Client;
-
-import java.util.ArrayList;
-
 /**
  * Created by Beata Iłowiecka on 12.03.2016.
  */
 public class Picture extends AbstractProduct {
 
-    public Picture(String name, String number, double price, String[] tags, boolean active) {
-        super(name, number, price, tags, active);
+    public Picture(String number, double price, String[] tags, boolean active) {
+        super(number, price, tags, active);
     }
 
-    public Picture(String name, String number, double price, String[] tags) {
-        this(name, number, price, tags, true);
+    public Picture(String number, double price, String[] tags) {
+        this(number, price, tags, true);
     }
 
     public Picture() {
-        this("programming", "nr4", 13, new String[]{"mouse", "screen", "coffee", "crayons"}, true);
+        this("nr4", 13, new String[]{"mouse", "screen", "coffee", "crayons"}, true);
     }
-
 }
--- src/pl/com/bottega/photostock/sales/model/products/Song.java
+++ src/pl/com/bottega/photostock/sales/model/products/Song.java
@@ -12,8 +12,8 @@ public class Song extends AbstractProduct{
     private Duration duration;
     private Channel channel;
 
-    public Song(String name, String number, double price, String[] tags, String performer, String title, Duration duration, Channel channel, boolean active) {
-        super(name, number, price, tags, active);
+    public Song(String number, double price, String[] tags, String performer, String title, Duration duration, Channel channel, boolean active) {
+        super(number, price, tags, active);
         this.performer = performer;
         this.title = title;
         this.duration = duration;
@@ -21,11 +21,11 @@ public class Song extends AbstractProduct{
 
     }
 
-    public Song(String name, String number, double price, String[] tags, String performer, String title, Duration duration, Channel channel) {
-        this(name, number, price, tags, performer, title, duration, channel, true);
+    public Song(String number, double price, String[] tags, String performer, String title, Duration duration, Channel channel) {
+        this(number, price, tags, performer, title, duration, channel, true);
     }
 
     public Song(){
-        this("Gold on the ceiling", "nr6", 12, new String[] {"black keys", "ceiling", "gold"}, "Black keys", "Gold on the ceiling", Duration.ofSeconds(225), Channel.STEREO);
+        this("nr6", 12, new String[] {"black keys", "ceiling", "gold"}, "Black keys", "Gold on the ceiling", Duration.ofSeconds(225), Channel.STEREO);
     }
 }
