3e72556f67102c1a14bf6c30486cdef9d2a7f98a
ksrubka <beata.choluj@wp.pl>
2016-05-22 11:46:34 +0200

    Create shouldThrowDataAccessExceptionWhenFileNotFound()

--- test/pl/com/bottega/photostock/sales/infrastructure/repository/FileProductRepositoryTest.java
+++ test/pl/com/bottega/photostock/sales/infrastructure/repository/FileProductRepositoryTest.java
@@ -2,15 +2,18 @@ package pl.com.bottega.photostock.sales.infrastructure.repository;
 
 import org.junit.Assert;
 import org.junit.Test;
+import pl.com.bottega.photostock.sales.infrastructure.repositories.DataAccessException;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.FileProductRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.ProductRepository;
 import pl.com.bottega.photostock.sales.model.Money;
 import pl.com.bottega.photostock.sales.model.Product;
+import pl.com.bottega.photostock.sales.model.products.Clip;
 import pl.com.bottega.photostock.sales.model.products.Picture;
 
 import java.io.IOException;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -19,7 +22,7 @@ import static org.junit.Assert.assertTrue;
 public class FileProductRepositoryTest {
 
     @Test
-    public void shouldLoadProduct() throws IOException {
+    public void shouldLoadProduct() {
         //given
         ProductRepository productRepository =
                 new FileProductRepository(getClass().getResource("/fixtures/products.csv").getPath());
@@ -31,5 +34,21 @@ public class FileProductRepositoryTest {
         assertEquals(new Money(5.0, "USD"), product.getPrice());
         assertTrue(product.isAvailable());
         assertEquals(new String[] {"tag1", "tag2"}, ((Picture) product).getTags());
+    }
+
+    @Test
+    public void shouldThrowDataAccessExceptionWhenFileNotFound() {
+        //given
+        ProductRepository productRepository = new FileProductRepository("Fake path");
+        //when
+        DataAccessException ex = null;
+        try {
+            productRepository.load("nr2");
+        }
+        catch (DataAccessException dae) {
+            ex = dae;
+        }
+        //then
+        assertNotNull(ex);
     }
 }
