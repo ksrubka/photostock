7a297c00abb4c027aac4b320e7b327abfe3d61d5
ksrubka <beata.choluj@wp.pl>
2016-05-26 22:42:04 +0200

    Implement load(lightBoxNr)

--- src/pl/com/bottega/photostock/sales/infrastructure/repositories/file/FileLightBoxRepository.java
+++ src/pl/com/bottega/photostock/sales/infrastructure/repositories/file/FileLightBoxRepository.java
@@ -1,7 +1,14 @@
 package pl.com.bottega.photostock.sales.infrastructure.repositories.file;
 
 import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.LightBoxRepository;
+import pl.com.bottega.photostock.sales.model.Client;
 import pl.com.bottega.photostock.sales.model.LightBox;
+import pl.com.bottega.photostock.sales.model.exceptions.DataAccessException;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by Beata Iłowiecka on 24.05.16.
@@ -16,6 +23,23 @@ public class FileLightBoxRepository implements LightBoxRepository {
 
     @Override
     public LightBox load(String number) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path), 512)) {
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.trim().length() == 0)
+                    return null;
+                LightBox lightBox = parseLightBox(line);
+                if (lightBox.getNumber().equals(number))
+                    return lightBox;
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e);
+        }
+        return null;
+    }
+
+    private LightBox parseLightBox(String line) {
         return null;
     }
 
