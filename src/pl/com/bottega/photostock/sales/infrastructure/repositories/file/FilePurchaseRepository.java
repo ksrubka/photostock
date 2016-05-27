057e9ba39dfb246f3f79c52163ade1c5db70d1c4
ksrubka <beata.choluj@wp.pl>
2016-05-27 19:59:17 +0200

    Implement load(purchaseNr)

--- src/pl/com/bottega/photostock/sales/infrastructure/repositories/file/FilePurchaseRepository.java
+++ src/pl/com/bottega/photostock/sales/infrastructure/repositories/file/FilePurchaseRepository.java
@@ -3,8 +3,12 @@ package pl.com.bottega.photostock.sales.infrastructure.repositories.file;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ClientRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ProductRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.PurchaseRepository;
+import pl.com.bottega.photostock.sales.model.LightBox;
 import pl.com.bottega.photostock.sales.model.Purchase;
+import pl.com.bottega.photostock.sales.model.exceptions.DataAccessException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.List;
 import java.util.Set;
 
@@ -25,6 +29,19 @@ public class FilePurchaseRepository implements PurchaseRepository{
 
     @Override
     public Purchase load(String number) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path), 512)) {
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.trim().length() == 0)
+                    return null;
+                Purchase purchase = parsePurchase(line);
+                if (purchase.getNumber().equals(number))
+                    return purchase;
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e);
+        }
         return null;
     }
 
