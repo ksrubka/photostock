d70a1463a5c3df06e8549b79ebe17ce80fa3e762
ksrubka <beata.choluj@wp.pl>
2016-05-27 19:17:33 +0200

    Init fields in constructor

--- src/pl/com/bottega/photostock/sales/infrastructure/repositories/file/FilePurchaseRepository.java
+++ src/pl/com/bottega/photostock/sales/infrastructure/repositories/file/FilePurchaseRepository.java
@@ -1,5 +1,7 @@
 package pl.com.bottega.photostock.sales.infrastructure.repositories.file;
 
+import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ClientRepository;
+import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ProductRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.PurchaseRepository;
 import pl.com.bottega.photostock.sales.model.Purchase;
 
@@ -10,6 +12,16 @@ import java.util.Set;
  * Created by Beata Iłowiecka on 24.05.16.
  */
 public class FilePurchaseRepository implements PurchaseRepository{
+
+    private final String path;
+    ClientRepository clientRepository;
+    ProductRepository productRepository;
+
+    public FilePurchaseRepository(String path, ClientRepository clientRepository, ProductRepository productRepository) {
+        this.path = path;
+        this.clientRepository = clientRepository;
+        this.productRepository = productRepository;
+    }
 
     @Override
     public Purchase load(String number) {
