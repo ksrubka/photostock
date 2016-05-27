462a0b99d4ff6d3d42f1b35f0a2c5560542b9623
ksrubka <beata.choluj@wp.pl>
2016-05-27 18:53:18 +0200

    create @Before initializing fields

--- test/pl/com/bottega/photostock/sales/infrastructure/repositories/FilePurchaseRepositoryTest.java
+++ test/pl/com/bottega/photostock/sales/infrastructure/repositories/FilePurchaseRepositoryTest.java
@@ -1,7 +1,24 @@
 package pl.com.bottega.photostock.sales.infrastructure.repositories;
 
+import org.junit.Before;
+import pl.com.bottega.photostock.sales.infrastructure.repositories.file.FileClientRepository;
+import pl.com.bottega.photostock.sales.infrastructure.repositories.file.FileProductRepository;
+import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ClientRepository;
+import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ProductRepository;
+
 /**
  * Created by Beata Iłowiecka on 24.05.16.
  */
 public class FilePurchaseRepositoryTest {
+
+    String path;
+    ClientRepository clientRepository;
+    ProductRepository productRepository;
+
+    @Before
+    public void initVariables() {
+        path = "test/fixtures/lightboxes.csv";
+        clientRepository = new FileClientRepository("test/fixtures/clients.csv");
+        productRepository = new FileProductRepository("test/fixtures/products.csv");
+    }
 }
