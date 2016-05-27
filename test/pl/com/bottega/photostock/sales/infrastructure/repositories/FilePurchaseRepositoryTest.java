e0fd27ee323d6b39297a04fd4268ef03ed8b24ae
ksrubka <beata.choluj@wp.pl>
2016-05-27 19:54:04 +0200

    Create shouldWritePurchases()

--- test/pl/com/bottega/photostock/sales/infrastructure/repositories/FilePurchaseRepositoryTest.java
+++ test/pl/com/bottega/photostock/sales/infrastructure/repositories/FilePurchaseRepositoryTest.java
@@ -3,18 +3,19 @@ package pl.com.bottega.photostock.sales.infrastructure.repositories;
 import org.junit.Before;
 import org.junit.Test;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.file.FileClientRepository;
-import pl.com.bottega.photostock.sales.infrastructure.repositories.file.FileLightBoxRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.file.FileProductRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.file.FilePurchaseRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ClientRepository;
-import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.LightBoxRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ProductRepository;
 import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.PurchaseRepository;
+import pl.com.bottega.photostock.sales.model.Client;
 import pl.com.bottega.photostock.sales.model.Product;
 import pl.com.bottega.photostock.sales.model.Purchase;
 import pl.com.bottega.photostock.sales.model.exceptions.DataAccessException;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
@@ -72,5 +73,58 @@ public class FilePurchaseRepositoryTest {
         }
         //then
         assertNotNull(ex);
+    }
+
+    @Test
+    public void shouldWritePurchases() {
+        //given
+        String path = "tmp/purchases.csv";
+        PurchaseRepository purchaseRepository =
+                new FilePurchaseRepository(path, clientRepository, productRepository);
+        Purchase purchase1 = create1stPurchase();
+        Purchase purchase2 = create2ndPurchase();
+        //when
+        purchaseRepository.save(purchase1);
+        purchaseRepository.save(purchase2);
+        //then
+        Purchase purchase1Read = purchaseRepository.load("nr1");
+        Purchase purchase2Read = purchaseRepository.load("nr2");
+        //assertions
+        assertEquals("nr1", purchase1Read.getNumber());
+        assertEquals("nr2", purchase2Read.getNumber());
+        assertEquals("Pani Gosia", purchase1Read.getOwner().getName());
+        assertEquals("Pan Kuba", purchase2Read.getOwner().getName());
+        assertEquals("nr5", purchase1Read.getOwner().getNumber());
+        assertEquals("nr6", purchase2Read.getOwner().getNumber());
+        assertEquals("nr1 nr2", purchase1Read.getProductsNumbers());
+        assertEquals("nr3", purchase2Read.getProductsNumbers());
+        File file = new File("tmp/purchases.csv");
+        file.delete();
+    }
+
+    private Purchase create1stPurchase() {
+        Client client = clientRepository.load("nr5");
+        Product product1 = productRepository.load("nr1");
+        Product product2 = productRepository.load("nr2");
+        List<Product> items= new ArrayList<>();
+        items.add(product1);
+        items.add(product2);
+        Purchase purchase = new Purchase(client, items);
+        purchase.setNumber("nr1");
+        Date date = createDate();
+        purchase.setDate(date);
+        return purchase;
+    }
+
+    private Purchase create2ndPurchase() {
+        Client client = clientRepository.load("nr6");
+        Product product1 = productRepository.load("nr3");
+        List<Product> items= new ArrayList<>();
+        items.add(product1);
+        Purchase purchase = new Purchase(client, items);
+        purchase.setNumber("nr2");
+        Date date = createDate();
+        purchase.setDate(date);
+        return purchase;
     }
 }
