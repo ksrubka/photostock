f08d301039a993cf9925b22705c2dc990bffa518
ksrubka <beata.choluj@wp.pl>
2016-06-02 22:34:26 +0200

    Create JDBCClientRepositoryTest class

---
+++ test/pl/com/bottega/photostock/sales/infrastructure/repositories/real/JDBCClientRepositoryTest.java
@@ -0,0 +1,24 @@
+package pl.com.bottega.photostock.sales.infrastructure.repositories.real;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.com.bottega.photostock.sales.infrastructure.repositories.interfaces.ClientRepository;
+import pl.com.bottega.photostock.sales.model.Client;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Beata Iłowiecka on 02.06.16.
+ */
+public class JDBCClientRepositoryTest {
+
+    ClientRepository clientRepo;
+
+
+}
+
