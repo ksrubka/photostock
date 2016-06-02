73f011401ec40e290b38ce488648b53d1c3839b0
ksrubka <beata.choluj@wp.pl>
2016-06-02 22:36:07 +0200

    Create setUp() in @Before
    with helper private methods: createClientsTable() and insertTestClient()

--- test/pl/com/bottega/photostock/sales/infrastructure/repositories/real/JDBCClientRepositoryTest.java
+++ test/pl/com/bottega/photostock/sales/infrastructure/repositories/real/JDBCClientRepositoryTest.java
@@ -19,6 +19,35 @@ public class JDBCClientRepositoryTest {
 
     ClientRepository clientRepo;
 
+    @Before
+    public void setUp() throws Exception {
+        Connection c = DriverManager.getConnection("jdbc:hsqldb:mem:stock", "SA", "");
+        createClientsTable(c);
+        insertTestClient(c);
+        clientRepo = new JDBCClientRepository("jdbc:hsqldb:mem:stock", "SA", "");
+        c.close();
+    }
 
+    private void createClientsTable(Connection c) throws Exception {
+        c.createStatement().executeUpdate("DROP TABLE Products IF EXISTS");
+        Statement statement = c.createStatement();
+        statement.executeUpdate("CREATE TABLE Clients (\n" +
+                "  id INTEGER IDENTITY PRIMARY KEY,\n" +
+                "  number VARCHAR(20) NOT NULL,\n" +
+                "  name VARCHAR(255) NOT NULL,\n" +
+                "  address VARCHAR(255) NOT NULL,\n" +
+                "  active BOOLEAN DEFAULT true NOT NULL,\n" +
+                "  amount INTEGER DEFAULT 0 NOT NULL,\n" +
+                "  currency CHAR(3) DEFAULT 'PLN' NOT NULL,\n" +
+                "  status VARCHAR(20) DEFAULT 'standard' NOT NULL,\n" +
+                ");");
+    }
+
+    private void insertTestClient(Connection c) throws Exception {
+        Statement statement = c.createStatement();
+        statement.executeUpdate(
+                "INSERT INTO Clients (number, name, address, active, amount, currency, status) " +
+                        "VALUES ('nr1', 'Anna Makara', 'Księżycowa 2/13', true, 2000, 'USD', 'vip');\n");
+    }
 }
 
