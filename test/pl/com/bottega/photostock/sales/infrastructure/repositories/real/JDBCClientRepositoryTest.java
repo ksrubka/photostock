b87c6583bd37e38dc4fcfc106f7ec22f4e3dd47d
ksrubka <beata.choluj@wp.pl>
2016-06-06 07:42:28 +0200

    Implement shouldUpdateClient()

--- test/pl/com/bottega/photostock/sales/infrastructure/repositories/real/JDBCClientRepositoryTest.java
+++ test/pl/com/bottega/photostock/sales/infrastructure/repositories/real/JDBCClientRepositoryTest.java
@@ -10,9 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by Beata Iłowiecka on 02.06.16.
@@ -98,5 +96,17 @@ public class JDBCClientRepositoryTest {
 
     @Test
     public void shouldUpdateClient() {
+        Client client = new Client("Helena Szczęsna", "Akacjowa 2", ClientStatus.SILVER, 200, "EUR", true, "nr2");
+        Client clientChanged = new Client("Helena Nieszczęsna", "Akacjowa 4", ClientStatus.VIP, 2000, "PLN", false, "nr2");
+        clientRepo.save(client);
+        clientRepo.save(clientChanged);
+        Client clientChangedLoaded = clientRepo.load("nr2");
+        assertEquals("Helena Nieszczęsna", clientChangedLoaded.getName());
+        assertEquals("Akacjowa 4", clientChangedLoaded.getAddress());
+        assertEquals(ClientStatus.VIP, clientChangedLoaded.getStatus());
+        assertEquals(2000, clientChangedLoaded.getSaldo().cents()/100);
+        assertEquals("PLN", String.valueOf(clientChangedLoaded.getSaldo().getCurrency()));
+        assertFalse(clientChangedLoaded.isActive());
+        assertEquals("nr2", clientChangedLoaded.getNumber());
     }
 }
